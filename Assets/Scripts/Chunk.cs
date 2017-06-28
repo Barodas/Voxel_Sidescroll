8dc2893431bfdfc33f2d2b40e42bc6383e85a521
Luke Savage <lsava22@hotmail.com>
2017-06-28 19:52:42 +1000

    Set up 2 layer terrain

---
+++ Assets/Scripts/Chunk.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshCollider))]
+public class Chunk : MonoBehaviour
+{
+    private MeshFilter _filter;
+    private MeshCollider _col;
+    private Block[,,] _blocks = new Block[chunkSize, chunkSize, chunkDepth]; // X, Y, Foreground/Background
+
+    public World world;
+    public WorldPos pos;
+    public static int chunkSize = 16; // X, Y
+    public static int chunkDepth = 2; // Foreground/Background
+    public bool update = true;
+
+    private void Start()
+    {
+        _filter = gameObject.GetComponent<MeshFilter>();
+        _col = gameObject.GetComponent<MeshCollider>();
+
+        // Example Chunk Generation
+        //_blocks = new Block[chunkSize, chunkSize, chunkDepth];
+        //for (int x = 0; x < chunkSize; x++)
+        //{
+        //    for (int y = 0; y < chunkSize; y++)
+        //    {
+        //        for (int z = 0; z < chunkDepth; z++)
+        //        {
+        //            _blocks[x, y, z] = new BlockAir();
+        //        }
+        //    }
+        //}
+        //_blocks[3, 5, 0] = new Block();
+        //_blocks[4, 5, 0] = new BlockGrass();
+        //UpdateChunk();
+        // End Example Chunk Generation
+    }
+
+    void Update()
+    {
+        if (update)
+        {
+            update = false;
+            UpdateChunk();
+        }
+    }
+
+    public Block GetBlock(int x, int y, int z)
+    {
+        if (InRange(x) && InRange(y) && InRange(z))
+            return _blocks[x, y, z];
+        return world.GetBlock(pos.x + x, pos.y + y, z);
+    }
+
+    public void SetBlock(int x, int y, int z, Block block)
+    {
+        if (InRange(x) && InRange(y) && InRange(z))
+        {
+            _blocks[x, y, z] = block;
+        }
+        else
+        {
+            world.SetBlock(pos.x + x, pos.y + y, z, block);
+        }
+    }
+
+    public static bool InRange(int index)
+    {
+        if (index < 0 || index >= chunkSize)
+            return false;
+
+        return true;
+    }
+
+    private void UpdateChunk()
+    {
+        MeshData meshData = new MeshData();
+        for (int x = 0; x < chunkSize; x++)
+        {
+            for (int y = 0; y < chunkSize; y++)
+            {
+                for (int z = 0; z < chunkDepth; z++)
+                {
+                    meshData = _blocks[x, y, z].Blockdata(this, x, y, z, meshData);
+                }
+            }
+        }
+        RenderMesh(meshData);
+    }
+
+    private void RenderMesh(MeshData meshData)
+    {
+        _filter.mesh.Clear();
+        _filter.mesh.vertices = meshData.vertices.ToArray();
+        _filter.mesh.triangles = meshData.triangles.ToArray();
+        _filter.mesh.uv = meshData.uv.ToArray();
+        _filter.mesh.RecalculateNormals();
+
+        _col.sharedMesh = null;
+        Mesh mesh = new Mesh();
+        mesh.vertices = meshData.colVertices.ToArray();
+        mesh.triangles = meshData.colTriangles.ToArray();
+        mesh.RecalculateNormals();
+
+        _col.sharedMesh = mesh;
+    }
+}
---
+++ Assets/Scripts/World.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+    public Dictionary<WorldPos, Chunk> chunks = new Dictionary<WorldPos, Chunk>();
+    public GameObject chunkPrefab;
+
+    void Start()
+    {
+        for (int x = -2; x < 2; x++)
+        {
+            for (int y = -1; y < 1; y++)
+            {
+                CreateChunk(x * 16, y * 16);
+            }
+        }
+    }
+
+    public Chunk GetChunk(int x, int y)
+    {
+        WorldPos pos = new WorldPos();
+        float multiple = Chunk.chunkSize;
+        pos.x = Mathf.FloorToInt(x / multiple) * Chunk.chunkSize;
+        pos.y = Mathf.FloorToInt(y / multiple) * Chunk.chunkSize;
+        Chunk containerChunk = null;
+        chunks.TryGetValue(pos, out containerChunk);
+
+        return containerChunk;
+    }
+    public Block GetBlock(int x, int y, int z)
+    {
+        Chunk containerChunk = GetChunk(x, y);
+        if (containerChunk != null)
+        {
+            Block block = containerChunk.GetBlock(x - containerChunk.pos.x, y - containerChunk.pos.y, z);
+
+            return block;
+        }
+        else
+        {
+            return new BlockAir();
+        }
+    }
+
+    public void SetBlock(int x, int y, int z, Block block)
+    {
+        Chunk chunk = GetChunk(x, y);
+
+        if (chunk != null)
+        {
+            chunk.SetBlock(x - chunk.pos.x, y - chunk.pos.y, z, block);
+            chunk.update = true;
+        }
+    }
+
+    public void CreateChunk(int x, int y)
+    {
+        //the coordinates of this chunk in the world
+        WorldPos worldPos = new WorldPos(x, y);
+
+        //Instantiate the chunk at the coordinates using the chunk prefab
+        GameObject newChunkObject = Instantiate(chunkPrefab, new Vector3(worldPos.x, worldPos.y), Quaternion.Euler(Vector3.zero)) as GameObject;
+
+        //Get the object's chunk component
+        Chunk newChunk = newChunkObject.GetComponent<Chunk>();
+
+        //Assign its values
+        newChunk.pos = worldPos;
+        newChunk.world = this;
+
+        //Add it to the chunks dictionary with the position as the key
+        chunks.Add(worldPos, newChunk);
+
+        //Add the following:
+        for (int xi = 0; xi < 16; xi++)
+        {
+            for (int yi = 0; yi < 16; yi++)
+            {
+                if (yi <= 7)
+                {
+                    SetBlock(x + xi, y + yi, 0, new BlockGrass());
+                    SetBlock(x + xi, y + yi, 1, new BlockGrass());
+                }
+                else
+                {
+                    SetBlock(x + xi, y + yi, 0, new BlockAir());
+                    SetBlock(x + xi, y + yi, 1, new BlockAir());
+                }
+            }
+        }
+    }
+
+    public void DestroyChunk(int x, int y)
+    {
+        Chunk chunk = null;
+        if (chunks.TryGetValue(new WorldPos(x, y), out chunk))
+        {
+            Object.Destroy(chunk.gameObject);
+            chunks.Remove(new WorldPos(x, y));
+        }
+    }
+}
